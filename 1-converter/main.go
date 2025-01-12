505de2951e8d79f540c6abbe62da53630ce56dbc
makarypereymak <91114399+makarypereymak@users.noreply.github.com>
2025-01-12 18:22:05 +0300

    Add function for getting user inputs and function template for conversion currencies

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -10,3 +10,22 @@ func main() {
 	eurToRub := (1 / usdtToEur) * usdtToRub
 	fmt.Print(eurToRub)
 }
+
+
+func getUserInputs () (string, string, float64) {
+	var initialCurrency string
+	var targetCurrency string
+	var initialNumber float64
+	fmt.Println("Enter initial currency: ")
+	fmt.Scan(&initialCurrency)	
+	fmt.Println("Enter target currency: ")
+	fmt.Scan(&targetCurrency)
+	fmt.Println("Enter initial number: ")	
+	fmt.Scan(&initialNumber)
+
+	return initialCurrency, targetCurrency, initialNumber
+}
+
+func convertCurrencies (initialCurrency string,  targetCurrency string, initialNumber float64) string {
+	return ""
+}
